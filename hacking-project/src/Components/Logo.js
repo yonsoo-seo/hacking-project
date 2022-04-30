c36bc1db2ab6a2cf4dd12cd785cf8dc5a7453015
yonsoo <ynsoo.seo@gmail.com>
2022-05-01 02:11:18 +0900

    main logo

---
+++ hacking-project/src/Components/Logo.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Logo.css";
+
+class Logo extends Component {
+  constructor(props) {
+    super(props);
+  }
+  state = {};
+  render() {
+    return <div className="logoname">WELCOME</div>;
+  }
+}
+
+export default Logo;
